779e4a2abd4942d46607f4c838de4d2415473f74
MaxsanAI <maxsansamurai@gmail.com>
2026-06-05 15:30:55 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://ai-hub-news.pages.dev', 
   integrations: [tailwind()],
 });
